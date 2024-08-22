3478d05490725ce84d2489b473167681acfe94ff
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-08-22 13:25:48 +0530

    LE2;
    main;

--- Labs/Exercise-2/main.c
+++ Labs/Exercise-2/main.c
@@ -2,17 +2,23 @@
 
 int main(void)
 {
+    // Test all the myput* functions
     myputs("Hello, World!\n");
     myputchar('A');
     myputchar('\n');
-    myputd(1230);
+    myputd(0);
+    myputchar('\n');
+    myputd(123);
     myputchar('\n');
     myputd(-456);
     myputchar('\n');
     myputx(0x789ABC);
     myputchar('\n');
-    myputf(123.456);
+    myputx(255);
+    myputchar('\n');
+    myputf(123.45);
 
+    // Print the buffer for now. Will be replaced later
     printf("%s", mybuf);
     return 0;
 }
