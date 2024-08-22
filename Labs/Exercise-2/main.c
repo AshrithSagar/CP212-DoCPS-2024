b0c1aa13275d0b3163eaad16ec2ecab9a46f4613
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-08-22 18:45:29 +0530

    LE2

--- Labs/Exercise-2/main.c
+++ Labs/Exercise-2/main.c
@@ -1,27 +1,46 @@
 #include "printlib.h"
 
+// Test all the myput* functions
 int main(void)
 {
-    // Test all the myput* functions
-    myputs("Hello, World!\n");
+    // Test printing a normal string
+    myputs("Hello, World!");
+    myputchar('\n');
+
+    // Test printing a single character
     myputchar('A');
     myputchar('\n');
+
+    // Test printing the number 0
     myputd(0);
     myputchar('\n');
+
+    // Test printing a positive decimal number
     myputd(123);
     myputchar('\n');
+
+    // Test printing a negative decimal number
     myputd(-456);
     myputchar('\n');
+
+    // Test printing a positive hexadecimal number, inputted in hexadecimal
     myputx(0x789ABC);
     myputchar('\n');
+
+    // Test printing a positive hexadecimal number, inputted in decimal
     myputx(127);
     myputchar('\n');
+
+    // Test printing a negative hexadecimal number, inputted in decimal
     myputx(-1);
     myputchar('\n');
+
+    // Test printing a floating point number
     myputf(123.45);
     myputchar('\n');
 
     // Print the buffer for now. Will be replaced later
     printf("%s", mybuf);
+
     return 0;
 }
