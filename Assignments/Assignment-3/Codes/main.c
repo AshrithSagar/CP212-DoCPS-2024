dd91277f958cee8d1741e4dbaca2278e9c9e35a3
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-08-28 14:44:20 +0530

    A3

--- Assignments/Assignment-3/Codes/main.c
+++ Assignments/Assignment-3/Codes/main.c
@@ -4,49 +4,49 @@
 int main(void)
 {
     // Test printing a single character
-    myprintf("c", 'A');
-    myprintf("c", '\n');
+    myprintf("%c", 'A');
+    myprintf("%c", '\n');
 
     // Test printing the number 0
-    myprintf("d", 0);
-    myprintf("c", '\n');
+    myprintf("%d", 0);
+    myprintf("%c", '\n');
 
     // Test printing a positive decimal number
-    myprintf("d", 123);
-    myprintf("c", '\n');
+    myprintf("%d", 123);
+    myprintf("%c", '\n');
 
     // Test printing a negative decimal number
-    myprintf("d", -456);
-    myprintf("c", '\n');
+    myprintf("%d", -456);
+    myprintf("%c", '\n');
 
     // Test printing a positive decimal number with trailing zeros
-    myprintf("d", 7890);
-    myprintf("c", '\n');
+    myprintf("%d", 7890);
+    myprintf("%c", '\n');
 
     // Test printing a positive hexadecimal number, inputted in hexadecimal
     // myputx(0x789ABC);
-    myprintf("d", 0xFF);
-    myprintf("c", '\n');
+    myprintf("%d", 0xFF);
+    myprintf("%c", '\n');
 
     // Test printing a negative hexadecimal number, inputted in hexadecimal
-    myprintf("x", -0xDEF);
-    myprintf("c", '\n');
+    myprintf("%x", -0xDEF);
+    myprintf("%c", '\n');
 
     // Test printing a positive hexadecimal number, inputted in decimal
-    myprintf("x", 127);
-    myprintf("c", '\n');
+    myprintf("%x", 127);
+    myprintf("%c", '\n');
 
     // Test printing a negative hexadecimal number, inputted in decimal
-    myprintf("x", -1);
-    myprintf("c", '\n');
+    myprintf("%x", -1);
+    myprintf("%c", '\n');
 
     // Test printing a positive floating point number
-    myprintf("f", 123.45);
-    myprintf("c", '\n');
+    myprintf("%f", 123.45);
+    myprintf("%c", '\n');
 
     // Test printing a negative floating point number
-    myprintf("f", -67.89);
-    myprintf("c", '\n');
+    myprintf("%f", -67.89);
+    myprintf("%c", '\n');
 
     // Print the buffer for now. Will be replaced later
     printf("%s", mybuf);
