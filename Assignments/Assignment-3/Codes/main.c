7a11154d5a4a6224cfda6b14094d1647b59618c1
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-08-28 14:25:02 +0530

    A3

--- Assignments/Assignment-3/Codes/main.c
+++ Assignments/Assignment-3/Codes/main.c
@@ -5,52 +5,52 @@ int main(void)
 {
     // Test printing a normal string
     myprintf("s", "Hello, World!");
-    myputchar('\n');
+    myprintf("c", '\n');
 
     // Test printing a single character
     myprintf("c", 'A');
-    myputchar('\n');
+    myprintf("c", '\n');
 
     // Test printing the number 0
     myprintf("d", 0);
-    myputchar('\n');
+    myprintf("c", '\n');
 
     // Test printing a positive decimal number
     myprintf("d", 123);
-    myputchar('\n');
+    myprintf("c", '\n');
 
     // Test printing a negative decimal number
     myprintf("d", -456);
-    myputchar('\n');
+    myprintf("c", '\n');
 
     // Test printing a positive decimal number with trailing zeros
     myprintf("d", 7890);
-    myputchar('\n');
+    myprintf("c", '\n');
 
     // Test printing a positive hexadecimal number, inputted in hexadecimal
     // myputx(0x789ABC);
     myprintf("d", 0xFF);
-    myputchar('\n');
+    myprintf("c", '\n');
 
     // Test printing a negative hexadecimal number, inputted in hexadecimal
     myprintf("x", -0xDEF);
-    myputchar('\n');
+    myprintf("c", '\n');
 
     // Test printing a positive hexadecimal number, inputted in decimal
     myprintf("x", 127);
-    myputchar('\n');
+    myprintf("c", '\n');
 
     // Test printing a negative hexadecimal number, inputted in decimal
     myprintf("x", -1);
-    myputchar('\n');
+    myprintf("c", '\n');
 
     // Test printing a positive floating point number
     myprintf("f", 123.45);
-    myputchar('\n');
+    myprintf("c", '\n');
 
     // Test printing a negative floating point number
     myprintf("f", -67.89);
-    myputchar('\n');
+    myprintf("c", '\n');
 
     return 0;
 }
