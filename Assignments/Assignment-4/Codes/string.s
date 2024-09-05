f604db3882d38f09a40561a99b9efbee087927c9
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-09-06 04:28:34 +0530

    A4

--- Assignments/Assignment-4/Codes/string.s
+++ Assignments/Assignment-4/Codes/string.s
@@ -1,10 +1,11 @@
 .syntax unified
 .text
 .global mystrlen
+.global mystrcpy
 
 
-; Return length of input string
 mystrlen:
+; Return length of input string
 
 	; Read string
 	MOV R1, #0				; Initialise length
@@ -18,4 +19,25 @@ mystrlen:
 
 _mystrlen__exit:
 	MOV R0, R1				; Return value in R0
+	BX LR					; Return
+
+
+mystrcopy:
+; Copy a source string to a destination
+	MOV R3, R0				; Save destination
+
+	; Copy string head
+	LDR R2, [R1]			; Read source string head
+	STR R2, [R0]			; Copy to destination
+
+	; Continue?
+	CMP R2, #0				; Check Null character
+	BEQ _mystrcopy__exit	; Exit reading loop
+
+	; Increment string pointers
+	ADDS R1, #1
+	ADDS R0, #1
+
+_mystrcopy__exit:
+	MOV R0, R3				; Return value in R0
 	BX LR					; Return
