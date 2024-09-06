6478002d40de6f6bc958e93842302853c488d20e
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-09-06 10:00:24 +0530

    A4

--- Assignments/Assignment-4/Codes/string.s
+++ Assignments/Assignment-4/Codes/string.s
@@ -6,45 +6,45 @@
 
 
 mystrlen:
-; Return length of input string
+// Return length of input string
 
-	; Read string
-	MOV R1, #0				; Initialise length
-	LDR R2, [R0]			; Read string head
-	CMP R2, #0				; Check Null character
-	BEQ _mystrlen__exit		; Exit reading loop
+	// Read string
+	MOV R1, #0				// Initialise length
+	LDR R2, [R0]			// Read string head
+	CMP R2, #0				// Check Null character
+	BEQ _mystrlen__exit		// Exit reading loop
 
-	; Update
-	ADDS R1, R1, #1			; Increment length
-	ADDS R0, R0, #1			; Increment string pointer
+	// Update
+	ADDS R1, R1, #1			// Increment length
+	ADDS R0, R0, #1			// Increment string pointer
 
 _mystrlen__exit:
-	MOV R0, R1				; Return value in R0
-	BX LR					; Return
+	MOV R0, R1				// Return value in R0
+	BX LR					// Return
 
 
 mystrcopy:
-; Copy a source string to a destination
-	MOV R3, R0				; Save destination
+// Copy a source string to a destination
+	MOV R3, R0				// Save destination
 
 _mystrcopy_loop:
-	; Copy string head
-	LDR R2, [R1]			; Read source string head
-	STR R2, [R0]			; Copy to destination
+	// Copy string head
+	LDR R2, [R1]			// Read source string head
+	STR R2, [R0]			// Copy to destination
 
-	; Continue?
-	CMP R2, #0				; Check Null character
-	BEQ _mystrcopy__exit	; Exit reading loop
+	// Continue?
+	CMP R2, #0				// Check Null character
+	BEQ _mystrcopy__exit	// Exit reading loop
 
-	; Increment string pointers
+	// Increment string pointers
 	ADDS R1, R1, #1
 	ADDS R0, R0, #1
 
-	B _mystrcopy_loop		; Continue string copying
+	B _mystrcopy_loop		// Continue string copying
 
 _mystrcopy__exit:
-	MOV R0, R3				; Return value in R0
-	BX LR					; Return
+	MOV R0, R3				// Return value in R0
+	BX LR					// Return
 
 
 mystrcmp:
@@ -52,35 +52,32 @@ mystrcmp:
 Return 0 if strings are equal, else return the difference
 in characters at their first point of difference */
 
-	MOV R4, #0				; Result
+	MOV R4, #0				// Result
 
 _mystrcmp_loop:
-	; Read strings
-	LDR R2, [R0]			; Read string1 head
-	LDR R3, [R1]			; Read string2 head
+	// Read strings
+	LDR R2, [R0]			// Read string1 head
+	LDR R3, [R1]			// Read string2 head
 
-	; Compare
+	// Compare
 	CMP R2, R3
 	BNE _mystrcmp__diff
 
-	; Check Null character
+	// Check Null character
 	CMP R2, #0
 	BEQ _mystrcmp__equal
 
-	; Increment string pointers
+	// Increment string pointers
 	ADDS R0, R0, #1
 	ADDS R1, R1, #1
 
-	B _mystrcmp_loop		; Continue string parsing
+	B _mystrcmp_loop		// Continue string parsing
 
-_mystrcmp__equal:			; Identical strings
-	MOV R0, #0				; Return 0
-	B _mystrcmp__return
+_mystrcmp__equal:			// Identical strings
+	MOV R0, #0				// Return 0
+	BX LR					// Return
 
 _mystrcmp__diff:
-; Compute string difference at first non-matching character
-	SUBS R4, R3, R2
-
-_mystrcmp__return:
-	MOV R0, R4				; Return value in R0
-	BX LR					; Return
+// Compute string difference at first non-matching character
+	SUBS R0, R3, R2
+	BX LR					// Return
