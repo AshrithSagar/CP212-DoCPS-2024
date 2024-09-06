c133eacef6d3aac18c33d5e9d949f48e413b71f5
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-09-07 01:31:35 +0530

    A4

--- Assignments/Assignment-4/Codes/main.c
+++ Assignments/Assignment-4/Codes/main.c
@@ -1,13 +1,22 @@
 #include "string.h"
-#include <stdio.h>
+
+int len1, len2;
+long testmem[10];
+
+char src[] = "Welcome to mystrcpy.";
+char dest[32]; // sufficiently large to hold the copy
 
 int main(void) {
-  char mystring[] = "Hello World!";
-  char newstring[BUFSIZ];
+  len1 = mystrlen("Hi!");
+  len2 = mystrlen("Hello, world!");
 
-  printf("%d\n", mystrlen(mystring));
-  printf("%s\n", mystrcpy(newstring, mystring));
-  printf("%d", mystrcmp(mystring, "Hello world?"));
+  mystrcpy(dest, src);
+
+  testmem[0] = mystrcmp("Apple", "Mango");
+  testmem[1] = mystrcmp("Mango", "Apple");
+  testmem[2] = mystrcmp("Bang", "Bangalore");
+  testmem[3] = mystrcmp("Bangalore", "Bang");
+  testmem[4] = mystrcmp("Rose", "Rose");
 
   return 0;
 }
