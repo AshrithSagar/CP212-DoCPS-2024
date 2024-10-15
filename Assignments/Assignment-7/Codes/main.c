cb948dc68e05cae1d3b914d9e286dc54d6f14a84
Ashrith Sagar <ashrithy@iisc.ac.in>
2024-10-16 04:45:43 +0530

    A7

--- Assignments/Assignment-7/Codes/main.c
+++ Assignments/Assignment-7/Codes/main.c
@@ -4,53 +4,51 @@
 #include "button.h"
 #include "display.h"
 
-#define NUM_PICS 5
+#define NUM_PICS 3
 char pics[NUM_PICS][N][N] = {
-    {{1, 1, 1, 1, 1},
-     {1, 0, 0, 0, 1},
-     {1, 0, 1, 0, 1},
-     {1, 0, 0, 0, 1},
-     {1, 1, 1, 1, 1}},
-
-    {{0, 0, 1, 0, 0},
-     {0, 1, 1, 1, 0},
-     {1, 1, 1, 1, 1},
-     {0, 1, 1, 1, 0},
-     {0, 0, 1, 0, 0}},
-
-    {{1, 0, 1, 0, 1},
-     {0, 1, 0, 1, 0},
-     {1, 0, 1, 0, 1},
-     {0, 1, 0, 1, 0},
-     {1, 0, 1, 0, 1}},
-
-    {{0, 1, 0, 1, 0},
-     {1, 0, 1, 0, 1},
-     {0, 1, 1, 1, 0},
-     {1, 0, 1, 0, 1},
-     {0, 1, 0, 1, 0}},
-
-    {{1, 0, 0, 0, 1},
-     {0, 1, 0, 1, 0},
-     {0, 0, 1, 0, 0},
-     {0, 1, 0, 1, 0},
-     {1, 0, 0, 0, 1}},
+    // Blank
+    {
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0},
+    },
+    // Left arrow
+    {
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0},
+        {1, 1, 1, 1, 1},
+        {0, 1, 1, 1, 0},
+        {0, 0, 1, 0, 0},
+    },
+    // Right arrow
+    {
+        {0, 0, 1, 0, 0},
+        {0, 0, 1, 1, 1},
+        {1, 1, 1, 1, 1},
+        {0, 0, 1, 1, 1},
+        {0, 0, 1, 0, 0},
+    },
 };
 
 int main(void) {
+  int currentPic;
+
   displayInit();
   buttonInit();
 
-  int currentPic;
-  currentPic = 0;
-
   while (1) {
-    displayImage(pics[currentPic]);
-
     if (BUTTON_PRESSED(BTN_A)) {
-      currentPic = (currentPic + 1) % NUM_PICS;
+      currentPic = 1;
       naiveDelay(100); // Debounce
+    } else if (BUTTON_PRESSED(BTN_B)) {
+      currentPic = 2;
+      naiveDelay(100); // Debounce
+    } else {
+      currentPic = 0;
     }
+    displayImage(pics[currentPic]);
   }
 
   return 0;
