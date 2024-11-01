d1c1558058eb8301052b062c62acfb65fe049da5
Ashrith Sagar <ashrithy@iisc.ac.in>
2024-11-01 18:55:36 +0530

    A9

---
+++ Assignments/Assignment-9/Codes/printlib.h
@@ -0,0 +1,12 @@
+#ifndef PRINTLIB_H // Include Guard: Prevent multiple inclusions
+#define PRINTLIB_H
+
+// Function prototypes
+int myputchar(int c);
+int myputs(const char *s);
+int myputd(int d);
+int myputx(int d);
+int myputf(float f);
+int myprintf(const char *format, ...);
+
+#endif // PRINTLIB_H
