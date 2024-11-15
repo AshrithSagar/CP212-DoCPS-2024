c28002a00f1376b9fed7656e0d2f87710822675f
Ashrith Sagar <ashrithy@iisc.ac.in>
2024-11-16 01:11:54 +0530

    P1

--- Projects/Codes/encoder.c
+++ Projects/Codes/encoder.c
@@ -16,21 +16,25 @@ void encoder_init(int M1E1, int M1E2, int M2E1, int M2E2) {
   uart_init();
 }
 
+void handle_edge(int *counter, int *lastState, int currentState) {
+  if (*lastState == LOW && currentState == HIGH) {
+    (*counter)++; // Rising edge
+    myprintf("Counter: %d\n", *counter);
+  } else if (*lastState == HIGH && currentState == LOW) {
+    (*counter)++; // Falling edge
+    myprintf("Counter: %d\n", *counter);
+  }
+  *lastState = currentState;
+}
+
 void encoder_counter(void) {
   static int counter = 0;
-  static int lastState = LOW;
-  int current;
+  static int prevStateM1 = HIGH;
+  static int prevStateM2 = HIGH;
 
   int val_M1E1 = digitalRead(pinM1E1);
   int val_M2E1 = digitalRead(pinM2E1);
 
-  current = val_M1E1;
-  if (lastState == LOW && current == HIGH) {
-    counter++; // Rising edge
-    myprintf("Counter: %d\n", counter);
-  } else if (lastState == HIGH && current == LOW) {
-    counter++; // Falling edge
-    myprintf("Counter: %d\n", counter);
-  }
-  lastState = current;
+  handle_edge(&counter, &prevStateM1, val_M1E1);
+  handle_edge(&counter, &prevStateM2, val_M2E1);
 }
