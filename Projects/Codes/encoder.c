5ad5bc05265c9efe57ab85c1e9db139349f327df
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-11-22 00:55:02 +0530

    P1

--- Projects/Codes/encoder.c
+++ Projects/Codes/encoder.c
@@ -7,14 +7,18 @@
 #define NUM_MS_IN_MIN (60000000L) // Microseconds in a minute
 #define PPR 325                   // Pulses per revolution
 
-unsigned long timer_m1, timer_m1_prev, timer_m1_diff;
-unsigned long timer_m2, timer_m2_prev, timer_m2_diff;
+typedef struct {
+  unsigned long current;
+  unsigned long previous;
+  unsigned long diff;
+} Timer;
 
 typedef struct {
   int pinE1, pinE2;
   int counter;
   int lastStateE1;
   int lastStateE2;
+  Timer timer;
 } Motor;
 
 void Motor_init(Motor *motor1, Motor *motor2, int M1E1, int M1E2, int M2E1,
@@ -22,30 +26,29 @@ void Motor_init(Motor *motor1, Motor *motor2, int M1E1, int M1E2, int M2E1,
   motor1->pinE1 = M1E1;
   motor1->pinE2 = M1E2;
   motor1->counter = 0;
+  motor1->timer.current = 0;
+  motor1->timer.previous = 0;
+  motor1->timer.diff = 0;
 
   motor2->pinE1 = M2E1;
   motor2->pinE2 = M2E2;
   motor2->counter = 0;
+  motor2->timer.current = 0;
+  motor2->timer.previous = 0;
+  motor2->timer.diff = 0;
 
   pinMode(M1E1, INPUT, PULLDOWN);
   digitalInterruptEnable(M1E1, GPIO_RISINGEDGE, 0);
 
   pinMode(M2E1, INPUT, PULLDOWN);
   digitalInterruptEnable(M2E1, GPIO_RISINGEDGE, 1);
-
-  timer_m1 = 0;
-  timer_m1_prev = 0;
-  timer_m1_diff = 0;
-  timer_m2 = 0;
-  timer_m2_prev = 0;
-  timer_m2_diff = 0;
 }
 
 float Motor_getSpeed(Motor *motor, int pin) {
-  if (pin == motor->pinE1 && timer_m1_diff != 0)
-    return ((float)NUM_MS_IN_MIN / timer_m1_diff) / PPR;
-  else if (pin == motor->pinE2 && timer_m2_diff != 0)
-    return ((float)NUM_MS_IN_MIN / timer_m2_diff) / PPR;
+  if (pin == motor->pinE1 && motor->timer.diff != 0)
+    return ((float)NUM_MS_IN_MIN / motor->timer.diff) / PPR;
+  else if (pin == motor->pinE2 && motor->timer.diff != 0)
+    return ((float)NUM_MS_IN_MIN / motor->timer.diff) / PPR;
   else
     return 0;
 }
@@ -69,13 +72,13 @@ void Motor_updateCounter(Motor *motor1, Motor *motor2, int event) {
 
 void Motor_updateEncoder(Motor *motor, int event) {
   if (event == 0) {
-    timer_m1 = timer32_read();
-    timer_m1_diff = timer_m1 - timer_m1_prev;
-    timer_m1_prev = timer_m1;
+    motor->timer.current = timer32_read();
+    motor->timer.diff = motor->timer.current - motor->timer.previous;
+    motor->timer.previous = motor->timer.current;
   } else if (event == 1) {
-    timer_m2 = timer32_read();
-    timer_m2_diff = timer_m2 - timer_m2_prev;
-    timer_m2_prev = timer_m2;
+    motor->timer.current = timer32_read();
+    motor->timer.diff = motor->timer.current - motor->timer.previous;
+    motor->timer.previous = motor->timer.current;
   }
 }
 
