bd261cc840d097996a8d84071aa416a97adf2b51
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-11-22 18:34:43 +0530

    P1

--- Projects/Codes/uart.c
+++ Projects/Codes/uart.c
@@ -1,68 +1,50 @@
 #include "uart.h"
+#include "nrf52833.h"
 #include <stdarg.h>
-
-#define IOREG(addr) (*((volatile long *)(addr)))
 
 #define UART_TX_PIN 6
 #define UART_RX_PIN 40
 // Incorrect the manual, which interchanges these
 
-#define UART_STARTTX IOREG(0x40002008)
-#define UART_STARTRX IOREG(0x40002000)
-#define UART_TXDRDY IOREG(0x4000211C)
-#define UART_RXDRDY IOREG(0x40002108)
-#define UART_ENABLE IOREG(0x40002500)
-#define UART_PSEL_TXD IOREG(0x4000250C)
-#define UART_PSEL_RXD IOREG(0x40002514)
-#define UART_TXD IOREG(0x4000251C)
-#define UART_RXD IOREG(0x40002518)
-#define UART_BAUDRATE IOREG(0x40002524)
-
 void uart_init() {
   // Make pin connection
-  UART_PSEL_TXD = UART_TX_PIN;
-  UART_PSEL_RXD = UART_RX_PIN;
+  NRF_UART0->PSEL.TXD = UART_TX_PIN;
+  NRF_UART0->PSEL.RXD = UART_RX_PIN;
 
-  // Set BAUD rate; 115200;
-  // => 11520 bytes per second, including 1 start bit + 8 data bits + 1 stop bit
-  UART_BAUDRATE = 0x01D7E000;
+  NRF_UART0->BAUDRATE = 0x01D7E000;
+  // BAUD rate; 115200; => 11520 bytes per second
+  // Comprises of 1 start bit + 8 data bits + 1 stop bit
 
-  // Start TX; Trigger task;
-  UART_STARTTX = 1;
-
-  // Start RX; Trigger task;
-  UART_STARTRX = 1;
-
-  // Enable UART
-  UART_ENABLE = 4;
-
+  NRF_UART0->TASKS_STARTTX = 1;
+  NRF_UART0->TASKS_STARTRX = 1;
+  NRF_UART0->ENABLE = 4;
   return;
 }
 
 char uart_putc(char ch) {
   // Set character to be transmitted
-  UART_TXD = ch;
+  NRF_UART0->TXD = ch;
 
   // Wait till TXDRDY is 1
-  while (UART_TXDRDY == 0)
+  while (NRF_UART0->EVENTS_TXDRDY == 0)
     ;
 
   // Clear TXDRDY
-  UART_TXDRDY = 0;
+  NRF_UART0->EVENTS_TXDRDY = 0;
 
   return ch;
 }
 
 char uart_getc(void) {
   // Wait till RXDRDY is 1
-  while (UART_RXDRDY == 0)
+  while (NRF_UART0->EVENTS_RXDRDY == 0)
     ;
 
   // Clear RXDRDY
-  UART_RXDRDY = 0;
+  NRF_UART0->EVENTS_RXDRDY = 0;
 
   // Return received character
-  return UART_RXD;
+  return NRF_UART0->RXD;
 }
 
 char *uart_fgets(char *buf, int bufsize) {
@@ -77,7 +59,7 @@ char *uart_fgets(char *buf, int bufsize) {
   buf[i] = '\0';
 
   // Clear any leftover characters in the UART buffer
-  while (UART_RXDRDY == 1) {
+  while (NRF_UART0->EVENTS_RXDRDY == 1) {
     uart_getc();
   }
 
