806f478818f5af28d14d4cd1754dd4355d8144ef
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-11-22 19:08:40 +0530

    P1

--- Projects/Codes/gpio.c
+++ Projects/Codes/gpio.c
@@ -6,22 +6,10 @@
 // Macros
 #define IOREG(addr) (*((volatile long *)(addr)))
 #define IOREG32(addr) (*((volatile uint32_t *)(uintptr_t)(addr)))
-#define GPIO_PORT(pin) ((pin) < 32 ? GPIO_P0 : GPIO_P1)
+#define GPIO_PORT(pin) ((pin) < 32 ? NRF_P0 : NRF_P1)
 #define GPIO_BIT(pin) ((pin) < 32 ? (pin) : ((pin) - 32))
-#define CLEAR(port, offset, bit) (IOREG(port + offset) &= ~(1UL << (bit)))
-#define SET(port, offset, bit) (IOREG(port + offset) |= (1UL << (bit)))
-
-// GPIO base addresses
-#define GPIO_P0 0x50000000UL
-#define GPIO_P1 0x50000300UL
-
-// GPIO registers offsets
-#define GPIO_OUT 0x504
-#define GPIO_IN 0x510
-#define GPIO_DIR 0x514
-
-// GPIO configuration registers
-#define GPIO_CONFIG(pin) (0x700 + (pin) * 4)
+#define GPIOTE_MODEEVENT (1)
+#define NVIC_ISER IOREG32(0xE000E100)
 
 void pinMode(int pin, int direction, int pull) {
   /*
@@ -31,19 +19,16 @@ void pinMode(int pin, int direction, int pull) {
    * pull: PULL_NONE, PULLDOWN, or PULLUP
    */
 
-  unsigned long port, bit;
-  port = GPIO_PORT(pin);
-  bit = GPIO_BIT(pin);
+  NRF_GPIO_Type *port = GPIO_PORT(pin);
+  uint32_t bit = GPIO_BIT(pin);
 
   if (direction == INPUT) {
-    CLEAR(port, GPIO_DIR, bit);
-    IOREG(port + GPIO_CONFIG(bit)) = pull;
+    port->DIRCLR &= ~(1UL << bit);
+    port->PIN_CNF[bit] = pull;
   } else if (direction == OUTPUT) {
-    SET(port, GPIO_DIR, bit);
-    IOREG(port + GPIO_CONFIG(bit)) = OUTPUT;
+    port->DIRSET |= (1UL << bit);
+    port->PIN_CNF[bit] = OUTPUT;
   }
-
-  return;
 }
 
 void digitalWrite(int pin, int value) {
@@ -53,32 +38,21 @@ void digitalWrite(int pin, int value) {
    * value: LOW or HIGH
    */
 
-  long port;
-  int bit;
-  port = GPIO_PORT(pin);
-  bit = GPIO_BIT(pin);
+  NRF_GPIO_Type *port = GPIO_PORT(pin);
+  uint32_t bit = GPIO_BIT(pin);
 
   if (value == LOW)
-    CLEAR(port, GPIO_OUT, bit);
+    port->OUTCLR &= ~(1UL << bit);
   else if (value == HIGH)
-    SET(port, GPIO_OUT, bit);
-
-  return;
+    port->OUTSET |= (1UL << bit);
 }
 
 int digitalRead(int pin) {
-  long port;
-  int bit, read;
-  port = GPIO_PORT(pin);
-  bit = GPIO_BIT(pin);
+  NRF_GPIO_Type *port = GPIO_PORT(pin);
+  uint32_t bit = GPIO_BIT(pin);
 
-  read = ((IOREG(port + GPIO_IN) >> bit) & 0x01);
-  return read;
+  return (port->IN >> bit) & 0x01;
 }
-
-// GPIOTE & NVIC
-#define GPIOTE_MODEEVENT (1)
-#define NVIC_ISER IOREG32(0xE000E100)
 
 void digitalInterruptEnable(uint32_t pin, uint32_t edge, int event) {
   /* GPIOTE has 8 registers, each can be configured for event i (i = 0 to 7)
