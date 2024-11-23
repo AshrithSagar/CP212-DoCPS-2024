074e4d3ea2210f6d20dda15800953e586457460b
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-11-23 15:38:55 +0530

    P1

--- Projects/Codes/encoder.c
+++ Projects/Codes/encoder.c
@@ -4,8 +4,8 @@
 #include "printlib.h"
 #include "timer32.h"
 
-#define NUM_MS_IN_MIN (60000000L) // Microseconds in a minute
-#define PPR 325                   // Pulses per revolution
+#define NUM_MICROS_IN_MIN (60000000L) // Microseconds in a minute
+#define PPR 325                       // Pulses per revolution
 
 void configureEncoder(Motor *motor, int pinE1, int pinE2) {
   motor->encoder.pinE1 = pinE1;
@@ -19,19 +19,6 @@ void configureEncoder(Motor *motor, int pinE1, int pinE2) {
   digitalInterruptEnable(pinE1, GPIO_RISINGEDGE, motor->id);
 }
 
-float Motor_getSpeed(Motor *motor) {
-  if (motor->encoder.timer.diff != 0)
-    return ((float)NUM_MS_IN_MIN / motor->encoder.timer.diff) / PPR;
-  return 0.0;
-}
-
-float Motor_getRPM(Motor *motor) {
-  /* Calculate the RPM of the motor
-   * RPM = (Counter * 60) / PPR
-   */
-  return (float)(motor->encoder.counter * 60) / PPR;
-}
-
 void encoder_updateTimer(Motor *motor) {
   motor->encoder.timer.current = timer32_read();
   motor->encoder.timer.diff =
@@ -40,6 +27,24 @@ void encoder_updateTimer(Motor *motor) {
 }
 
 void encoder_updateCounter(Motor *motor) { motor->encoder.counter++; }
+
+float motor_getSpeed(Motor *motor) {
+  /* Calculate the speed of the motor
+   * rev/min = (microseconds per minute) / (
+   *  (microseconds per pulse) * (pulses per revolution))
+   */
+
+  if (motor->encoder.timer.diff != 0)
+    return ((float)NUM_MICROS_IN_MIN / motor->encoder.timer.diff) / PPR;
+  return 0.0;
+}
+
+float motor_getRPM(Motor *motor) {
+  /* Calculate the RPM of the motor
+   * RPM = (Counter * 60) / PPR
+   */
+  return (float)(motor->encoder.counter * 60) / PPR;
+}
 
 Motor motorLeft, motorRight;
 void encoder_init(StackBot *bot) {
@@ -54,6 +59,7 @@ void encoder_update(int event) {
   Motor *motor = (event == 0) ? &motorLeft : &motorRight;
   encoder_updateTimer(motor);
   encoder_updateCounter(motor);
-  myprintf("Motor 1: %d\tMotor 2: %d\n", motorLeft.encoder.counter,
-           motorRight.encoder.counter);
+
+  myprintf("Motor 1: %f\tMotor 2: %f\n", motor_getSpeed(&motorLeft),
+           motor_getSpeed(&motorRight));
 }
