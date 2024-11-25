1ba4080c1b02e60a40df28cea94dc2d16089dab5
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-11-25 13:05:35 +0530

    T3:3

--- Assignments/EndSem/Codes/q3-fix.c
+++ Assignments/EndSem/Codes/q3-fix.c
@@ -1,12 +1,13 @@
 /* This function computes a polynomial y = sum(Ci * x^i) for i = 0 to N. */
 
 #include <math.h>
+#include <stdio.h>
 
 #define N 4
-const int c[4] = {1234, 5678, 0x9ABC, 0xDEF0};
+const int c[N] = {1234, 5678, 0x9ABC, 0xDEF0};
 
 double poly(double x) {
-  double y;
+  double y = 0.0;
 
   for (int i = 0; i < N; i++)
     y += c[i] * pow(x, i);
